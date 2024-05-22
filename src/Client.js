9170ce31f03dabde89796e1d2e301c514cab11c6
rbkayz <rbkayz@gmail.com>
2024-05-22 21:02:33 +0530

    fix: removed framenavigated on logout

--- src/Client.js
+++ src/Client.js
@@ -337,13 +337,13 @@ class Client extends EventEmitter {
         await this.inject();
 
         this.pupPage.on('framenavigated', async (frame) => {
-            if(frame.url().includes('post_logout=1') || this.lastLoggedOut) {
-                this.emit(Events.DISCONNECTED, 'LOGOUT');
-                await this.authStrategy.logout();
-                await this.authStrategy.beforeBrowserInitialized();
-                await this.authStrategy.afterBrowserInitialized();
-                this.lastLoggedOut = false;
-            }
+            // if(frame.url().includes('post_logout=1') || this.lastLoggedOut) {
+            //     this.emit(Events.DISCONNECTED, 'FRAME NAVIGATED');
+            //     await this.authStrategy.logout();
+            //     await this.authStrategy.beforeBrowserInitialized();
+            //     await this.authStrategy.afterBrowserInitialized();
+            //     this.lastLoggedOut = false;
+            // }
             await this.inject(true);
         });
     }
